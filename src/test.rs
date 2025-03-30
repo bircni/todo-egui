372944ebc1ddddd7f8b090fd4934f9c6468151d7
Nicolas <bircni@icloud.com>
2025-03-30 15:37:03 +0200

    fix: change test-dir directory name

--- src/test.rs
+++ src/test.rs
@@ -4,6 +4,9 @@ use crate::data::{Category, Item, List};
 
 #[test]
 fn test_list() {
+    fs::create_dir_all("test-dir").unwrap_or_else(|e| {
+        panic!("Failed to create test directory: {e:?}");
+    });
     let mut list = List::default();
     assert_eq!(list.categories.len(), 0);
     list.categories.push(Category::default());
@@ -18,14 +21,14 @@ fn test_list() {
     list.categories[0].items.push(item);
     assert_eq!(list.categories[0].items.len(), 1);
 
-    let write_result = list.write(Path::new("test.json"), false);
+    let write_result = list.write(Path::new("test-dir/test.json"), false);
     assert!(
         write_result.is_ok(),
         "Failed to write list to file: {:?}",
         write_result.err()
     );
-    let load_result = list.load(Path::new("test.json"));
-    fs::remove_file("test.json").unwrap_or_else(|e| {
+    let load_result = list.load(Path::new("test-dir/test.json"));
+    fs::remove_file("test-dir/test.json").unwrap_or_else(|e| {
         panic!("Failed to remove test file: {e:?}");
     });
     assert!(
@@ -42,15 +45,15 @@ fn test_list() {
     assert_eq!(list.categories[0].items[0].notes, "");
 
     // create new file
-    let write_result = list.write(Path::new("test.json"), true);
+    let write_result = list.write(Path::new("test-dir/test.json"), true);
     assert!(
         write_result.is_ok(),
         "Failed to write list to file: {:?}",
         write_result.err()
     );
 
-    let load_result = list.load(Path::new("test.json"));
-    fs::remove_file("test.json").unwrap_or_else(|e| {
+    let load_result = list.load(Path::new("test-dir/test.json"));
+    fs::remove_file("test-dir/test.json").unwrap_or_else(|e| {
         panic!("Failed to remove test file: {e:?}");
     });
     assert!(
@@ -63,4 +66,8 @@ fn test_list() {
         list.categories.is_empty(),
         "List should be empty after new file"
     );
+
+    fs::remove_dir_all("test-dir").unwrap_or_else(|e| {
+        panic!("Failed to remove test directory: {e:?}");
+    });
 }
